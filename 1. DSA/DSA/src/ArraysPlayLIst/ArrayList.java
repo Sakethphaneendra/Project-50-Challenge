0a5ba99cc19b9809b0c43b72affabac14002c675
Saketh Phaneendra Kondisetti <sakethphaneendra@gmail.com>
2024-05-13 23:57:19 +0530

    Day -7

---
+++ 1. DSA/DSA/src/ArraysPlayLIst/ArrayList.java
@@ -0,0 +1,15 @@
+package ArraysPlayLIst;
+import java.util.*;
+//Write a Java program to create an ArrayList of integers and add elements to it. Then, print out the contents of the ArrayList.
+
+public class ArrayList {
+	public void s() {
+		int ArrayList[] = new int[5];
+		Scanner in = new Scanner(System.in);
+		for(int i=0;i<8;i++) {
+			System.out.println("hi");
+		}
+	}
+	
+
+}
